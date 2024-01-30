ac4689437bab57039a3fd9030eff61d1ba0574d3
ehuntober <ehuntercrypto@gmail.com>
2024-01-30 17:06:11 +0100

    project setup added

---
+++ scenes/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	var direction = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
+	velocity = direction * 200
+	move_and_slide()
+	
+	
+	if velocity.length() > 0.0:
+		get_node("$Player_move").play_walk_animation()
+	#else:
+		#get_node("HappyBoo").play_idle_animation()
+
---
+++ scripts/Menu.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/trash_game.tscn")
+	
+func _on_option_pressed():
+	get_tree().change_scene_to_file("res://scenes/Option_menu.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
