702c91f116c91078a5fcf8eddc4b0dde8c5a80e6
ehuntober <ehuntercrypto@gmail.com>
2024-01-30 18:36:24 +0100

    git added new sprite animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var anim = $AnimatedSprite2D
+
 func _physics_process(delta):
 	var direction = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	velocity = direction * 200
@@ -7,7 +9,22 @@ func _physics_process(delta):
 	
 	
 	if velocity.length() > 0.0:
-		get_node("$Player_move").play_walk_animation()
+		#get_node("$Player_move").play_walk_animation()
+		anim.play("Idle")
 	#else:
 		#get_node("HappyBoo").play_idle_animation()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
